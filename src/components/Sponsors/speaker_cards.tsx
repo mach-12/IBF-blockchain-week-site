a6e4dcdd079adfdfba6b61b058c006a61e0e3a61
Nithin-Varma <mnithin1422@gmail.com>
2023-04-10 14:05:41 +0530

    remaining speakers

--- src/components/Sponsors/speaker_cards.tsx
+++ src/components/Sponsors/speaker_cards.tsx
@@ -6,6 +6,9 @@ import Loveneesh from "/public/speakers/LoveneeshDhir.jpg"
 import Zeel from "/public/speakers/ZeelPatel.png"
 import Anmol from "/public/speakers/AnmolArora.png"
 import Himanshu from "/public/speakers/Himanshu.png"
+import Satyarth from "/public/speakers/Satyarth.jpg"
+import Prakash from "/public/speakers/Prakash.jpg"
+
 
 const copy_link = () => {
   navigator.clipboard.writeText("vimal.kumar@bennett.edu.in")
@@ -206,8 +209,8 @@ export default function SpeakerCards() {
         <div className="max-w-xs rounded-xl overflow-hidden shadow-lg m-4 bg-gray-900">
           <Image className="w-3/4 rounded-full mx-auto mt-4" src={Anmol} alt="Anmol " />
           <div className="px-6 py-4 text-center">
-            <h2 className="font-bold text-xl mb-2 text-white">  </h2>
-            <p className="text-gray-400 text-base">evRel / Lead @ Polygon-Flipkart Blockchain Center Of Excellence</p>
+            <h2 className="font-bold text-xl mb-2 text-white">Anmol Arora</h2>
+            <p className="text-gray-400 text-base">DevRel / Lead @ Polygon-Flipkart Blockchain Center Of Excellence</p>
           </div>
 
           <div className="px-6 pb-5 flex flex-grow justify-around">
@@ -290,6 +293,68 @@ export default function SpeakerCards() {
 
           </div>
         </div>
+
+
+        {/* Prakarsh Pathak */}
+        <div className="max-w-xs rounded-xl overflow-hidden shadow-lg m-4 bg-gray-900">
+          <Image className="w-3/4 rounded-full mx-auto mt-4" src={Prakash} alt="Anmol " />
+          <div className="px-6 py-4 text-center">
+            <h2 className="font-bold text-xl mb-2 text-white">Prakarsh Pathak</h2>
+            <p className="text-gray-400 text-base">Developer Advocate @ Spheron</p>
+          </div>
+
+          <div className="px-6 pb-5 flex flex-grow justify-around">
+            <a rel="noopener noreferrer" href="https://www.linkedin.com/in/prakarshpathak/" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white hover:text-pink-400" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+                <path d="M8 11l0 5"></path>
+                <path d="M8 8l0 .01"></path>
+                <path d="M12 16l0 -5"></path>
+                <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+              </svg>
+            </a>
+
+            <a href="https://twitter.com/blockchainbalak">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white hover:text-pink-400"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z"></path>
+            </svg>
+            </a>
+
+          </div>
+        </div>
+
+        {/* Kumar Satyarth */}
+        <div className="max-w-xs rounded-xl overflow-hidden shadow-lg m-4 bg-gray-900">
+          <Image className="w-3/4 rounded-full mx-auto mt-4" src={Satyarth} alt="Anmol " />
+          <div className="px-6 py-4 text-center">
+            <h2 className="font-bold text-xl mb-2 text-white">Kumar Satyarth</h2>
+            <p className="text-gray-400 text-base">DevRel</p>
+          </div>
+
+          <div className="px-6 pb-5 flex flex-grow justify-around">
+            <a rel="noopener noreferrer" href="https://www.linkedin.com/in/ksatyarth2/" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white hover:text-pink-400" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+                <path d="M8 11l0 5"></path>
+                <path d="M8 8l0 .01"></path>
+                <path d="M12 16l0 -5"></path>
+                <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+              </svg>
+            </a>
+
+            <a href="https://twitter.com/ksatyarth2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white hover:text-pink-400"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z"></path>
+            </svg>
+            </a>
+
+          </div>
+        </div>
+
       
     </div>
 
